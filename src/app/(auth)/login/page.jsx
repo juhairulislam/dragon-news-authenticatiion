b322f45ba5aeb947fb878e5ff498a5290a0bfc65
juhairulislam <juhairulislam2018@gmail.com>
2026-04-27 07:31:11 +0600

    design login form

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,34 @@
+import Link from 'next/link';
 import React from 'react';
 
 const LoginPage = () => {
+
+    
+
     return (
-        <div>
-            Login
+        <div className='container mx-auto min-h-[80vh] flex justify-center items-center'>
+            <div className='p-20 rounded-xl bg-black'>
+
+<h1 className='font-bold text-2xl text-center mb-6'>Login Your Account</h1>
+
+<form className='space-y-4'>
+
+    <fieldset className="fieldset">
+  <legend className="fieldset-legend">Email</legend>
+  <input type="email" className="input" placeholder="Your Email" />
+</fieldset>
+    <fieldset className="fieldset">
+  <legend className="fieldset-legend">Password</legend>
+  <input type="password" className="input" placeholder="Your Password" />
+</fieldset>
+
+<button className='btn w-full bg-slate-50 text-black'>Login</button>
+
+
+</form>
+
+<p className='mt-6'>Don't Have An Account ? <Link href={'/register'} className='text-red-500'>Register</Link></p>
+            </div>
         </div>
     );
 };
