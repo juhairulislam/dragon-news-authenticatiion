5b77fdbf315026953f09adaa27e9b4edd47f37b2
juhairulislam <juhairulislam2018@gmail.com>
2026-04-27 07:42:59 +0600

    fundamental form function

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,18 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
 
 const LoginPage = () => {
 
-    
+    const handleLoginFunc = (e) =>{
+        e.preventDefault() ;
+
+        const email = e.target.email.value ;
+        const password = e.target.password.value ;
+
+        console.log(email, password)
+
+    }
 
     return (
         <div className='container mx-auto min-h-[80vh] flex justify-center items-center'>
@@ -11,15 +20,15 @@ const LoginPage = () => {
 
 <h1 className='font-bold text-2xl text-center mb-6'>Login Your Account</h1>
 
-<form className='space-y-4'>
+<form className='space-y-4' onSubmit={handleLoginFunc}>
 
     <fieldset className="fieldset">
   <legend className="fieldset-legend">Email</legend>
-  <input type="email" className="input" placeholder="Your Email" />
+  <input name='email' type="email" className="input" placeholder="Your Email" />
 </fieldset>
     <fieldset className="fieldset">
   <legend className="fieldset-legend">Password</legend>
-  <input type="password" className="input" placeholder="Your Password" />
+  <input name='password' type="password" className="input" placeholder="Your Password" />
 </fieldset>
 
 <button className='btn w-full bg-slate-50 text-black'>Login</button>
