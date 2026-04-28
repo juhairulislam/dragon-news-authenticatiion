e92c8e663657bbc90ef57b25d88623fa6b0374cb
juhairulislam <juhairulislam2018@gmail.com>
2026-04-28 19:08:42 +0600

    implement google sign in

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -1,12 +1,23 @@
+'use client'
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const RightSidebar = () => {
+
+  const handleSignInGoogle =async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+
+    console.log(data, 'data')
+  }
+
   return (
     <div>
       <h2 className="font-bold text-lg mb-4">Login with</h2>
       <div className="flex flex-col gap-2">
-        <button className="btn border-blue-500 text-blue-500">
+        <button className="btn border-blue-500 text-blue-500" onClick={handleSignInGoogle}>
           <FaGoogle />
           Login with google
         </button>
