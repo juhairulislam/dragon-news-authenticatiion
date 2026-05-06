8d442637861edf3908e8cf961ab28fcba5d0b4c0
juhairulislam <juhairulislam2018@gmail.com>
2026-05-06 08:51:43 +0600

    gap increase in right side bar

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -23,7 +23,7 @@ const RightSidebar = () => {
   return (
     <div>
       <h2 className="font-bold text-lg mb-4">Login with</h2>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-3">
         <button className="btn border-blue-500 text-blue-500" onClick={handleSignInGoogle}>
           <FaGoogle />
           Login with google
