1c72abb19b8d5704f8da8a9bec211c129649f104
juhairulislam <juhairulislam2018@gmail.com>
2026-05-06 08:55:04 +0600

    padding y axis added in header main

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -5,7 +5,7 @@ import { format } from "date-fns";
 
 const Header = () => {
   return (
-    <div className="text-center py-8 space-y-2">
+    <div className="text-center py-9 space-y-2">
       <Image
         src={logo}
         width={300}
